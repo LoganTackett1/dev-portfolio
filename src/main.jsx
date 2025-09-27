ab92d873f1425cc5cd19c7a4b7edc74999ef28a2
Logan Tackett <logantackettwork@gmail.com>
2025-09-27 15:15:41 -0600

    imported tesseract animation from previous repo

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import TesseractPortfolio from './TesseractPortfolio.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <TesseractPortfolio />
   </StrictMode>,
 )
---
+++ src/tesseract/index.js
@@ -0,0 +1,63 @@
+import $ from "jquery";
+import css from "./index.css";
+import { FOUR } from "./tesseract";
+
+const canvas = document.getElementById("tesseract");
+const canvasSize = Math.min(window.innerWidth, window.innerHeight);
+canvas.width = canvasSize;
+canvas.height = canvasSize;
+
+window.addEventListener("resize", () => {
+  const min = Math.min(window.innerWidth, window.innerHeight);
+  canvas.width = min;
+  canvas.height = min;
+});
+
+const ctx = canvas.getContext("2d");
+
+const Tesseract = FOUR.create4DCube();
+let wAngle = 0;
+const wDelta = 0.003;
+
+function amountscrolled() {
+  const winheight = $(window).height();
+  const docheight = $(document).height();
+  const scrollTop = $(window).scrollTop();
+  const trackLength = docheight - winheight;
+  const pctScrolled = scrollTop / trackLength;
+  return pctScrolled;
+}
+
+const navBar = document.getElementById("navbar");
+let currPercScroll = amountscrolled();
+let camAngle = currPercScroll * -Math.PI;
+window.addEventListener("scroll", () => {
+  const navY = navBar.getBoundingClientRect().y;
+  if (navY <= 10) {
+    navBar.classList.add("sticky-true");
+  } else {
+    navBar.classList.remove("sticky-true");
+  }
+
+  currPercScroll = amountscrolled();
+  camAngle = currPercScroll * -Math.PI;
+});
+
+const navBarBtn = document.getElementById("navbar-button");
+const navContainer = document.getElementById("navbar-container");
+navBarBtn.addEventListener("click", () => {
+  navContainer.classList.toggle("nav-off");
+  navBarBtn.classList.toggle("nav-off");
+});
+
+setInterval(() => {
+  FOUR.update3DMap(Tesseract, wAngle, 3);
+  const matrix = FOUR.getCenteringMatrix([
+    0,
+    Math.cos(camAngle),
+    Math.sin(camAngle),
+  ]);
+  FOUR.update2DMap(Tesseract, matrix, 3.7);
+  FOUR.drawCube(ctx, canvas, Tesseract);
+  wAngle += wDelta;
+}, 16);
